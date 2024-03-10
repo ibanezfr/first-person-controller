379de5f16e80aaf2add6a0136a6364bf16db878d
ibanezfr <francoibanez.dev@gmail.com>
2024-03-10 19:22:19 -0300

    add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# First Person Controller
+
+Assets folder: https://github.com/lukky-nl/FPS-controller-assets
+
--- player.gd
+++ player.gd
@@ -1,11 +1,35 @@
 extends CharacterBody3D
+# Player nodes
+@onready var head = $head
+@onready var standing_collision_shape = $standing_collision_shape
+@onready var crouching_collision_shape = $crouching_collision_shape
+@onready var ray_cast_3d = $RayCast3D
 
-
+# Speed variables
 var current_speed = 5.0
 var walking_speed = 5.0
 const sprinting_speed = 8.0
 const crouching_speed = 3.0
 const jump_velocity = 4.5
+
+# Movement variables
+const lerp_speed = 15.0
+var crouching_depth = -0.5
+
+# Input variables
+var direction = Vector3.ZERO
+const mouse_sens = 0.25
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	# Mouse looking logic
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(- event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(- event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,10 +37,29 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	
-	if Input.is_action_pressed("sprint"):
-		current_speed = sprinting_speed
-	else:
-		current_speed = walking_speed
+	# Handles movement state
+	# Crouching
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y, 1.69  + crouching_depth, delta*lerp_speed)
+		
+		standing_collision_shape.disabled = true
+		crouching_collision_shape.disabled = false
+	
+	elif !ray_cast_3d.is_colliding():
+		# Standing
+		head.position.y = lerp(head.position.y, 1.69, delta*lerp_speed)
+		
+		standing_collision_shape.disabled = false
+		crouching_collision_shape.disabled = true
+		
+		# Sprinting
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprinting_speed
+			
+		# Walking
+		else:
+			current_speed = walking_speed
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -29,7 +72,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
